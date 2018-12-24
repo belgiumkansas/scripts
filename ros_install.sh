1bc62cdcce803aa80df423e49a555a731350c26c
belgiumkansas <jeve0658@colorado.edu>
2018-12-23 21:19:53 -0700

    update

--- ros_install.sh
+++ ros_install.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
 sudo apt install ros-kinetic-desktop-full
 sudo rosdep init
@@ -12,8 +12,5 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 # install the catkin tools
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt update
 sudo apt install python-catkin-tools
 sudo apt install python-rosdep
